d6e313228a233f268ea7cd446992f81cd7235662
xyy931101 <18850198317@163.com>
2021-04-10 16:33:22 +0800

    fix:hashMap

---
+++ algorithm/src/week2/HashMapDemo.java
@@ -0,0 +1,18 @@
+package week2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: Xiongyy
+ * @Date: 2021/4/10 16:04
+ * .............................................
+ * 佛祖保佑             永无BUG
+ */
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap(16);
+        map.put(1, "1");
+    }
+}
